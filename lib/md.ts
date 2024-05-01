8cb035ec0aa91df93be439581b9c6c228b552d71
Seang-G <mgw7730@gmail.com>
2024-05-01 10:01:36 +0900

    delete useless

--- lib/md.ts
+++
@@ -1,7 +0,0 @@
-import { remark } from "remark";
-import remarkGfm from "remark-gfm";
-
-export default async function mdToHtml(md: string){
-  const result = await remark().use(remarkGfm).process(md);
-  return result.toString();
-}
